1ff9087ebb61c52082b9e9e29b0133516f8fa77c
jay <thecapacity@gmail.com>
2015-03-07 10:05:08 -0500

    Rendering Shiny Plots

--- DevData_Products/ui.R
+++ DevData_Products/ui.R
@@ -6,24 +6,13 @@
 library(shiny)
 
 shinyUI(pageWithSidebar(
-    headerPanel("Illustrating inputs"),
-    sidebarPanel(
-        numericInput('id1', "Numeric input, labeled id1", 0, min=0, max=10, step=1),
-        checkboxGroupInput("id2", "Checkbox", 
-                        c("Value 1" = "1",
-                          "Value 2" = "2",
-                          "Value 3" = "3")
-                        ),
-        dateInput("date", "Date:")
-        ),
-    mainPanel(
-        h3("Illustrating outputs"),
-        h4("You Entered"),
-        verbatimTextOutput("oid1"),
-        h4("You entered"),
-        verbatimTextOutput("oid2"),
-        h4("You N-tered"),
-        verbatimTextOutput("odate")
-    )
-))
+    headerPanel("Example plot"), 
 
+    sidebarPanel( 
+        sliderInput('mu', 'Guess at the mean', value = 70, min = 62, max = 74, step = 0.05,)
+    ), 
+
+    mainPanel( 
+        plotOutput('newHist')) 
+    )
+)

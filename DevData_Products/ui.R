0ac1acfa75993cc4e64fdfea72e76a0481a253b3
jay <thecapacity@gmail.com>
2015-03-07 09:46:56 -0500

    revisions and start/stop caveat.

--- DevData_Products/ui.R
+++ DevData_Products/ui.R
@@ -1,12 +1,22 @@
 #Run via Console `runApp()`
+#Doesn't end when I close the opened tab (i.e. console is still blocked)
+#I have to hit the <Stop> Button to get back command.
+
 library(shiny)
 
 shinyUI(pageWithSidebar(
-    headerPanel("Data science FTW!"),
+    headerPanel("Illustrating markup"),
     sidebarPanel(
-            h3("Sidebar tex")
+        h1("Sidebar panel tex"),
+        h1("H1 text"),
+        h2("H2 text"),
+        h3("H3 text"),
+        h4("H4 text")
     ),
     mainPanel(
-            h3("Main Panel text")
+            h3("Main Panel text"),
+            code("some code"),
+            p("some ordinary text")
     )
 ))
+

0c35f020c9dfa52f2ced48108cff9bc6508df115
jay <thecapacity@gmail.com>
2015-04-18 09:31:29 -0400

    Regression Mod Quizs

--- regmods-quizes/Quiz2.R
+++ regmods-quizes/Quiz2.R
@@ -9,6 +9,7 @@ y <- c(0.67, 0.84, 0.6, 0.18, 0.85, 0.47, 1.1, 0.65, 0.36)
 fit <- lm(y~x)
 summary(fit) #look for p-value (last line)
 summary(fit)$coefficients
+"0.05296"
 
 #Confirmed R's p-Value is two sided
 # https://stat.ethz.ch/R-manual/R-patched/library/stats/html/summary.lm.html
@@ -43,6 +44,7 @@ rownames(coefTable)<-c("(Intercept)","x")
 # From Binder1.pdf page 76
 sqrt(sum(resid(fit)^2)/(n-2))
 summary(fit)$sigma
+"0.222"
 
 # I should have watched video 01_06_c before attempting
 
@@ -61,10 +63,13 @@ confint(car_fit, level=.95)
 #Gets us close
 predict(car_fit, newdata=data.frame(wt=mean(mtcars$wt)))
 
+"18.991"
 # Interval for expected MPG at Average Wgt means intercept
 
 ## Q4
 ?mtcars # [, 6]    wt	Weight (lb/1000)
+
+"The estimated expected change in mpg per 1,000 lb increase in weight."
 
 ## Q5
 
@@ -78,6 +83,7 @@ predict(car_fit, newdata=data.frame(wt=3000/1000), interval=("prediction"))
 #1 20.09062 13.77366 26.40759
 
 #Not sure why mine does not align, potential answers are 21.24 & 27.57
+"27.57"
 
 ## Q6
 
@@ -86,13 +92,17 @@ predict(car_fit, newdata=data.frame(wt=3000/1000), interval=("prediction"))
 # Just did the transform, oriignal example is mpg/1000lbs
 #   So multiply that by 2,000lbs/1Short_Ton to get mpg/ST
 #   becomes 2*previous
+fit6 <- lm(mpg~ I(wt/2), data=mtcars)
+confint(fit6, level=.95)
+"-12.973"
 
 library(datasets)
 Cars <- mtcars
 Model <- lm(mpg ~ wt, Cars)
 predict(Model,newdata = data.frame(wt=mean(Cars$wt)), interval='confidence')
-Try interval='prediction', level=0.95.
-newdata=data.frame(wt=mtcars$wt) or newdata=data.frame(wt=3)
+#Try interval='prediction', level=0.95.
+newdata=data.frame(wt=mtcars$wt) 
+#or newdata=data.frame(wt=3)
 
 confint(object, parm, level = 0.95, ...)
 I also used the coef[row,col]+c(−1,1)∗qt(1−α/2,df)∗coef[row,col] format from the slides to double check my answer.
@@ -101,6 +111,7 @@ I also used the coef[row,col]+c(−1,1)∗qt(1−α/2,df)∗coef[row,col] format
 # X * .01 (to convert CM to M we're dividing by 100)
 # which means means S is divided by .01
 # So in this case Slope goes up by 100
+"It would get multiplied by 100."
 
 ## Q8
 
@@ -110,7 +121,8 @@ lm(mpg~I(wt), mtcars)
 lm(mpg~I(wt-10), mtcars)
 
 #I got this wrong the first time: Adding C to X makes the intercept change B0-C*B1
-# 
+
+"The new intercept would be β^0−cβ^1"
 
 ## Q9
 # anova(fit)
@@ -121,7 +133,7 @@ yhat <- predict(car_fit1, newdata=data.frame(wt=mtcars$wt))
 ymean <- mean(yhat)
 
 sum((mtcars$mpg-yhat)^2) / sum((mtcars$mpg-ymean)^2)
-
+"0.25"
 # yhat <- predict(car_fit1, newdata=data.frame(wt=mtcars$wt))
 # Then get ymean. 
 # The result is sum of (mtcars$mpg-yhat)^2 over sum of (mtcars$mpg-ymean)^2.
@@ -145,5 +157,6 @@ can understand.
 """
 
 ## Q10
+"If an intercept is included, then they will sum to 0."
 
 ## Answered based on the slides
